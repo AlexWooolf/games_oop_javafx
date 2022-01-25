4fef725dda114b9d18ee316662a7ec213e321583
Vovk <para_bellum@list.ru>
2022-01-25 10:21:47 +0300

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -3,33 +3,34 @@ package ru.job4j.puzzle;
 public class Win {
 
     public static boolean check(int[][] board) {
-        return checkRow(board) || checkColumn(board);
+        boolean result = false;
+        for (int row = 0; row < board.length; row++) {
+            if ( checkRow(board, row) || checkColumn(board, row) ) {
+                result = true;
+            }
+        }
+        return result;
     }
 
-    public static boolean checkRow(int[][] board) {
+    public static boolean checkRow(int[][] board, int row) {
         boolean rsl = true;
-        for (int row = 0; row < board.length - 1; row++) {
-            for (int cell = 0; cell < board[row].length - 1; cell++) {
-                if ((board[row][cell] != 1) && (board[row][cell] != board[row][cell + 1])) {
+        for (int cell = 0; cell < board[row].length; cell++) {
+                if (board[row][cell] != 1)  {
                     rsl = false;
                     break;
                 }
             }
-        }
-
         return rsl;
     }
-    public static boolean checkColumn(int[][] board) {
+
+    public static boolean checkColumn(int[][] board, int cell) {
         boolean rsl = true;
-        for (int row = 0; row < board.length - 1; row++) {
-            for (int cell = 0; cell < board[row].length - 1; cell++) {
-                if ((board[row][cell] != 1) && (board[row][cell] != board[row + 1][cell])) {
+        for (int row = 0; row < board.length ; row++) {
+                if (board[row][cell] != 1) {
                     rsl = false;
                     break;
                 }
             }
-        }
-
         return rsl;
     }
 }
